c86afd0a72ef3570b449503494c3ad794a1e0e17
prasanna <prasanna.gunda06@gmail.com>
2023-05-09 14:47:20 +0000

    xxxx

--- Sample.sh
+++ Sample.sh
@@ -3,4 +3,8 @@
 #give your 100% effort
 #put your intrest and iprove your skills 
 #Dont ever stop it
-ls -l
+echo "#Dedication is important in every work
+#Focus on the thing
+#give your 100% effort
+#put your intrest and iprove your skills 
+#Dont ever stop it"
